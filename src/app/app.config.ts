bd149755999cf4c841e746de0fd527cb4e5260a8
yanmatos31 <yancarai@gmail.com>
2025-06-02 22:29:14 -0300

    alterações pagina 404

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,11 @@ import { routes } from './app.routes';
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AuthInterceptor } from './auth/aut-interceptor';
 import { provideServiceWorker } from '@angular/service-worker';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
@@ -16,5 +21,5 @@ export const appConfig: ApplicationConfig = {
           }), provideServiceWorker('ngsw-worker.js', {
             enabled: !isDevMode(),
             registrationStrategy: 'registerWhenStable:30000'
-          })]
+          }), provideFirebaseApp(() => initializeApp({ projectId: "xpe-hotels-f36af", appId: "1:561718932352:web:6ba969973221ba1548c765", storageBucket: "xpe-hotels-f36af.firebasestorage.app", apiKey: "AIzaSyCj5YHclfgiBxpgPT93TuE1uwW7bpKkdUE", authDomain: "xpe-hotels-f36af.firebaseapp.com", messagingSenderId: "561718932352" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())]
 };
